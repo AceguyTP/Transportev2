7f15c978b67c658ea6787898254d326501b43b87
AceguyTP <halo4568@gmail.com>
2024-06-18 15:42:29 -0600

    correciones

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('rutas', function () {
+    return "Estas son las rutas disponibles";
+});
+
+Route::get('rutas/{ruta}', function ($ruta) {
+    return "Esta ruta lleva a $ruta";
+});
